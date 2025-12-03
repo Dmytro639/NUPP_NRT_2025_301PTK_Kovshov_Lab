8d44f46c743e57d80517ce739dc79e18cc5201a3
Dmytro639 <dmitrijkovshov56@gmail.com>
2025-12-03 21:40:47 +0300

    PlantTag.cs

---
+++ lab3/models/PlantTag.cs
@@ -0,0 +1,12 @@
+namespace Plants.Infrastructure.Models;
+
+
+public class PlantTag
+{
+public int PlantId { get; set; }
+public Plant? Plant { get; set; }
+
+
+public int TagId { get; set; }
+public Tag? Tag { get; set; }
+}
