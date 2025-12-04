9139cec3669110aa5e85c9393e86f6287087b987
Dmytro639 <dmitrijkovshov56@gmail.com>
2025-12-04 16:36:07 +0300

    Plant.cs

---
+++ lab5/Plants_Infrastructure/PlantsApp/Entities/Plant.cs
@@ -0,0 +1,9 @@
+namespace PlantsApp.Entities
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+    }
+}
